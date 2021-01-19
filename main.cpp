ac22741bc42dc021cb6f9d579f3b87d6cc99a0f3
Skylar Cupit <skylarcupit@gmail.com>
2021-01-19 14:40:21 -0600

    Updated configuration with compiler specific flags

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
-// NOTE: If this is a GUI only program, add -mwindows (GCC flag) to the release build.
+/*
+  NOTE: If this is a GUI only program, add -mwindows (GCC flag) to release build.
+  Also, add -static-libgcc (GCC, Clang) and -static-libstdc++ (GCC, Clang)
+    if sending to another person.
+
+  -Og flag is "optimizing for debugging", -Os optimized for exe size.
+
+  Clang analyzers and sanitizers don't work on Windows sadly.
+
+  Add -Werror (GCC and Clang only) to treat all warnings as errors.
+  Add /WX flag (MSVC only) to treat all warnings as errors.
+  Use /Wall for even more warnings than /W4 (MSVC only).
+
+  MSVC /O2 optimizes for speed, while /O1 optimizes for size.
+*/
 int main(int argc, const char** argv) {
 
+  cout << "Hello World" << endl;
   return EXIT_SUCCESS;
 }
