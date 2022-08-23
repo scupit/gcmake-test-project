77942c5f5728e57e505ad4053a5c1fb8985094df
Skylar Cupit <Skylarcupit@gmail.com>
2022-08-22 20:48:57 -0500

    Added example of system-specific defines

--- basic-tests/main.cpp
+++ basic-tests/main.cpp
@@ -9,21 +9,6 @@
   #define GLOBAL_DEF "Not defined globally"
 #endif
 
-/*
-  NOTE: If this is a GUI only program, add -mwindows (GCC flag) to release
-  build. Also, add -static-libgcc (GCC, Clang) and -static-libstdc++ (GCC,
-  Clang) if sending to another person.
-
-  -Og flag is "optimizing for debugging", -Os optimized for exe size.
-
-  Clang analyzers and sanitizers don't work on Windows sadly.
-
-  Add -Werror (GCC and Clang only) to treat all warnings as errors.
-  Add /WX flag (MSVC only) to treat all warnings as errors.
-  Use /Wall for even more warnings than /W4 (MSVC only).
-
-  MSVC /O2 optimizes for speed, while /O1 optimizes for size.
-*/
 int main()
 {
   printBuildInfo();
@@ -31,7 +16,9 @@ int main()
   DllClass("Message printed using DLL class").printMessage();
   ToggleLibClass().printMessage();
 
-  std::cout << "\nGLOBAL_DEF:\t" GLOBAL_DEF << '\n' << std::endl;
+  std::cout << "\nGLOBAL_DEF:\t" GLOBAL_DEF << '\n';
+
+  std::cout << "Targeting windows?: " << TARGETING_WINDOWS <<  '\n';
 
   return EXIT_SUCCESS;
 }
