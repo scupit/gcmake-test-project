5a8b97ee435640ca168a934e087f7267c582f3b7
Skylar Cupit <Skylarcupit@gmail.com>
2022-09-28 19:03:52 -0500

    Use the new 'correct import path' for a library's entry file

--- reference-parent/subprojects/example/main.cpp
+++ reference-parent/subprojects/example/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "REFERENCE_PARENT/SomeItem.hpp"
+#include "REFERENCE_PARENT/reference-parent.hpp"
 
-int main(int argc, const char** argv) {
+int main() {
   SomeItem("This is printing using the library created by the parent project.").printName();
-
   return EXIT_SUCCESS;
 }
