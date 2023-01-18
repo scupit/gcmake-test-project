c08508b12e3a30181d65c8c84ce274373c996b5e
Skylar Cupit <Skylarcupit@gmail.com>
2023-01-18 17:01:52 -0600

    Added an example homepage.hpp documentation file

---
+++ basic-tests/docs/homepage.hpp
@@ -0,0 +1,14 @@
+/**
+* \mainpage Example home page
+* 
+* \section example Example section
+* This is a documentation example for "Hello world":
+* 
+* \code
+* #include <iostream>
+* 
+* int main() {
+*   std::cout << "Hello World!\\n";
+* }
+* \endcode
+*/
