29fa51445581ce6528f76ad1c518303d6d74342b
Skylar Cupit <Skylarcupit@gmail.com>
2023-10-30 16:02:23 -0500

    Updated raylib example main file to avoid main while loop when building for web.

--- using-raylib/main.c
+++ using-raylib/main.c
@@ -1,18 +1,36 @@
+// This is a modified version of the raylib web example file at
+// https://github.com/raysan5/raylib/blob/master/examples/core/core_basic_window_web.c
 #include <stdlib.h>
 #include "raylib.h"
+
+#ifdef EMSCRIPTEN
+  #include <emscripten/emscripten.h>
+#endif
+
+void updateAndDrawFrame(void);
 
 // This is raylib's basic example.
 // https://github.com/raysan5/raylib#basic-example
 int main(void) {
   InitWindow(800, 450, "Basic raylib window example");
 
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Some very cool text", 190, 200, 20, LIGHTGRAY);
-    EndDrawing();
-  }
+  // This explains why the main while loop should be avoided when building for web.
+  // https://github.com/raysan5/raylib/wiki/Working-for-Web-(HTML5)#4-setup-raylib-game-for-web
+  #ifdef EMSCRIPTEN
+    emscripten_set_main_loop(updateAndDrawFrame, 0, 1);
+  #else
+    while (!WindowShouldClose()) {
+      updateAndDrawFrame();
+    }
+  #endif
 
   CloseWindow();
 	return EXIT_SUCCESS;
+}
+
+void updateAndDrawFrame(void) {
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
+  DrawText("Some very cool text", 190, 200, 20, LIGHTGRAY);
+  EndDrawing();
 }
