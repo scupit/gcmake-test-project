26790e3ed52fb17cbbb8e166a40a1d9b790b72b8
Skylar Cupit <skylarcupit@gmail.com>
2025-08-17 02:26:26 -0500

    Updated project reference-parent to work with GCMake 1.7.0

--- reference-parent/subprojects/example/src/REFERENCE_PARENT/EXAMPLE/main.cpp
+++ reference-parent/subprojects/example/src/REFERENCE_PARENT/EXAMPLE/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "REFERENCE_PARENT/reference-parent.hpp"
+#include "REFERENCE_PARENT/lib.hpp"
 
 int main() {
   SomeItem("This is printing using the library created by the parent project.").printName();
